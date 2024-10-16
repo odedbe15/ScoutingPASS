c6dda1859bae2dc04324819b15841104eb3d2196
odedbe15 <odedb0105@gmail.com>
2024-10-16 11:15:00 +0300

    Updated AuthKey

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -1,7 +1,7 @@
 // TBAInterface funcitons to pull data from TheBlueAlliance.com
 var teams = null;
 var schedule = null;
-var authKey = "uTHeEfPigDp9huQCpLNkWK7FBQIb01Qrzvt4MAjh9z2WQDkrsvNE77ch6bOPvPb6";
+var authKey = "AsIt4IgDEw3sFg6XDBY5ER5SyFvWSRrooCzm31x550WKNNu86mxR0BzElP08br7v";
 /**
  * Get list of teams in event
  *
